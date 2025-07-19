b2cde00b21c0e5bbed7b84fa2debd6cb78704fee
EduardoEkka <edwardekka17@gmail.com>
2025-07-20 00:28:31 +0530

    fix: port number fixed

--- src/index.js
+++ src/index.js
@@ -23,5 +23,5 @@ app.use("/files", fileRoutes);
 app.use("/search", searchRoutes);
 app.use("/user", userRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
