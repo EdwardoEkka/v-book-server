c3d01c65485d0e32d6e469c55e785652091058c8
EduardoEkka <edwardekka17@gmail.com>
2025-07-20 00:38:52 +0530

    fix

--- src/index.js
+++ src/index.js
@@ -23,5 +23,5 @@ app.use("/files", fileRoutes);
 app.use("/search", searchRoutes);
 app.use("/user", userRoutes);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.RUN_PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
